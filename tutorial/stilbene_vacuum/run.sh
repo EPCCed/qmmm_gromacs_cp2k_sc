45fe8222f7394c6552a50b2e3af667d4be1068b8
Julien Sindt <57000579+jsindt@users.noreply.github.com>
2021-12-15 11:50:16 +0000

    Update run.sh
    Setting for 1 hr on 2 nodes

--- tutorial/stilbene_vacuum/run.sh
+++ tutorial/stilbene_vacuum/run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #SBATCH -J stilbene_vacuum
-#SBATCH --time=00:20:00
+#SBATCH --time=1:0:0
 
 # Replace [budget code] below with your budget code (e.g. ta036-username)
 #SBATCH --account=[budget code]
 #SBATCH --partition=standard
-#SBATCH --qos=short
-#SBATCH --reservation=shortqos
+#SBATCH --qos=standard
 
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --tasks-per-node=32
 #SBATCH --cpus-per-task=4
 
@@ -21,4 +20,4 @@ module load gromacs/2021.1+cp2k
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export OMP_PLACES=cores
 
-srun mdrun_cp2k -npme 0 -s stilbene.tpr 
+srun mdrun_cp2k -npme 0 -s stilbene.tpr
